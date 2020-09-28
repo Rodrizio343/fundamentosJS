36cd5f10ccfea53f09f8d5a65924778b8a094cb2
Rodrigo Martínez <rodrigozoot94@gmail.com>
2020-09-28 20:22:28 -0300

    Ternarios

---
+++ 25-ternario.js
@@ -0,0 +1,22 @@
+let activo = true;
+
+// let mensaje = "";
+
+// Forma tradicional con if
+
+// if (activo===true){
+//     mensaje = 'activo'
+// } else {
+//     mensaje = 'inactivo'
+// }
+
+//Forma ternaria
+
+// const mensaje = activo===true ? 'activo' : 'inactivo'
+
+//Si solo quiero evaluar si es true:
+
+const mensaje = activo && 'activo'
+
+
+console.log(mensaje)
